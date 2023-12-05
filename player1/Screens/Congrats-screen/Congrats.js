8448216e04ce03ea180ce282fb46d2b0152cd9fe
S1LB4 <113909639+S1LB4@users.noreply.github.com>
2023-12-05 09:58:03 -0500

    Add files via upload

--- player1/Screens/Congrats-screen/Congrats.js
+++ player1/Screens/Congrats-screen/Congrats.js
@@ -1,9 +1,16 @@
-import p5 from 'p5';
+
 function setup() {
     noCanvas();
-    //cuadrado blanco
-    let saveImage = createImg('./IMAGES/Rectangle 5.jpg', 'save');
-    saveImage.child(saveImage);
+    // Crear div contenedor
+    let bodyDiv = document.createElement("div");
+    bodyDiv.classList.add("body");
+    // Crear div de felicitaciones
+    let congratsDiv = document.createElement("div");
+    congratsDiv.classList.add("CONGRATS");
+
+    // Crear div del rectángulo
+    let rectangleDiv = document.createElement("div");
+    rectangleDiv.classList.add("rectangulo");
 
     // Crear elementos del encabezado
     let header = createDiv('');
@@ -14,22 +21,26 @@ function setup() {
     let paragraph = document.createElement("p");
     paragraph.innerText =
       "You completed the rappi quest with a new time mark, you won the discount code for the restaurant in where you scanned the QR";
+    let orangecheck = createImg('./IMAGES/Check.jpg', 'save');
+    orangecheck.child(orangecheck);
     // Crear cuppon
     let couponCode = document.createElement("h3");
     couponCode.innerText = "7269-1a19-1be1-082n";
 
-    let saveButton = createButton('');
-    saveButton.class('save');
-    saveButton.mousePressed(cambiarPagina);
+    let saveCouponBtn = createButton('');
+    saveCouponBtn.class('save');
+    saveCouponBtn.mousePressed(cambiarPagina);
 
     // Agregar elementos al DOM
-    document.body.appendChild(backButton.elt);
-    document.body.appendChild(header.elt);
-    saveImage.child(header);
-    header.child(title);
-    header.child(paragraph);
-    header.child(couponCode);
-    header.child(saveButton);
+    rectangleDiv.appendChild(title);
+    rectangleDiv.appendChild(paragraph);
+    rectangleDiv.appendChild(orangeCheck);
+    rectangleDiv.appendChild(couponCode);
+    congratsDiv.appendChild(rectangleDiv);
+    // Agregar elementos al DOM
+    congratsDiv.appendChild(this.saveCouponBtn);
+    bodyDiv.appendChild(congratsDiv);
+    document.body.appendChild(bodyDiv);
 
   }
 
@@ -39,3 +50,5 @@ function setup() {
   function cambiarPagina() {
     window.location.href = '../Cuppon-screen/p5.html';
   }
+// Inicializar la clase
+new CongratsScreen();
