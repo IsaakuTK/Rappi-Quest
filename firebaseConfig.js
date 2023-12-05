a264bde23c18452aafc0d7f404d3ec91f7442135
S1LB4 <113909639+S1LB4@users.noreply.github.com>
2023-12-05 09:20:43 -0500

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBd_gEGQP0vdsPh6DVvdhhAIJToKdXWHe8",
+    authDomain: "rappiquest.firebaseapp.com",
+    projectId: "rappiquest",
+    storageBucket: "rappiquest.appspot.com",
+    messagingSenderId: "859137851440",
+    appId: "1:859137851440:web:c0f7169934897f19a26df5",
+    measurementId: "G-68L075GYT3"
+  };
