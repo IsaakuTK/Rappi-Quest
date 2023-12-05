0dd9b5e524177192afa2ab291d785d14b5f85877
S1LB4 <113909639+S1LB4@users.noreply.github.com>
2023-12-05 09:58:47 -0500

    Add files via upload

--- player1/Screens/Sorry-screen/Sorry.js
+++ player1/Screens/Sorry-screen/Sorry.js
@@ -1,72 +1,53 @@
-// Importar la librería p5.js
-import p5 from 'p5';
 
-// Definir una clase para la pantalla de disculpa
-class SorryScreen {
-  constructor() {
-    // Inicializar variables necesarias
-    this.backToRappiBtn = null;
+function setup() {
+    noCanvas();
+   // Crear div contenedor
+   let bodyDiv = document.createElement("div");
+   bodyDiv.classList.add("body");
 
-    // Configurar funciones de p5.js
-    new p5(this.sketch);
+   // Crear div de disculpa
+   let sorryDiv = document.createElement("div");
+   sorryDiv.classList.add("SORRY");
+
+   // Crear div del rectángulo
+   let rectangleDiv = document.createElement("div");
+   rectangleDiv.classList.add("rectangulo");
+
+   // Crear elementos internos del rectángulo
+   let title = document.createElement("h1");
+   title.innerText = "Sorry";
+   let paragraph = document.createElement("p");
+   paragraph.innerText =
+     "You completed the rappi quest but you didn't beat the best times, try again later when you can play again";
+   let orangeCheck = document.createElement("img");
+   orangeCheck.classList.add("orangecheck");
+   orangeCheck.src = "./IMAGES/X.jpg";
+   orangeCheck.alt = "";
+
+   // Agregar elementos al DOM
+   rectangleDiv.appendChild(title);
+   rectangleDiv.appendChild(paragraph);
+   rectangleDiv.appendChild(orangeCheck);
+   sorryDiv.appendChild(rectangleDiv);
+
+   // Crear botón
+   this.backToRappiBtn = document.createElement("button");
+   this.backToRappiBtn.id = "BacktoRappibtn";
+   this.backToRappiBtn.innerText = "Back to Rappi";
+   this.backToRappiBtn.addEventListener("click", () => this.changePage());
+
+   // Agregar elementos al DOM
+   sorryDiv.appendChild(this.backToRappiBtn);
+   bodyDiv.appendChild(sorryDiv);
+   document.body.appendChild(bodyDiv);
+ }
+
+  function draw() {
   }
 
-  // Función de configuración de p5.js
-  sketch(p) {
-    p.setup = () => {
-      // Crear elementos y configurar eventos cuando el DOM esté completamente cargado
-      p.createCanvas(window.innerWidth, window.innerHeight);
-      this.createHTML();
-    };
-  }
-
-  createHTML() {
-    // Crear div contenedor
-    let bodyDiv = document.createElement("div");
-    bodyDiv.classList.add("body");
-
-    // Crear div de disculpa
-    let sorryDiv = document.createElement("div");
-    sorryDiv.classList.add("SORRY");
-
-    // Crear div del rectángulo
-    let rectangleDiv = document.createElement("div");
-    rectangleDiv.classList.add("rectangulo");
-
-    // Crear elementos internos del rectángulo
-    let title = document.createElement("h1");
-    title.innerText = "Sorry";
-    let paragraph = document.createElement("p");
-    paragraph.innerText =
-      "You completed the rappi quest but you didn't beat the best times, try again later when you can play again";
-    let orangeCheck = document.createElement("img");
-    orangeCheck.classList.add("orangecheck");
-    orangeCheck.src = "./IMAGES/X.jpg";
-    orangeCheck.alt = "";
-
-    // Agregar elementos al DOM
-    rectangleDiv.appendChild(title);
-    rectangleDiv.appendChild(paragraph);
-    rectangleDiv.appendChild(orangeCheck);
-    sorryDiv.appendChild(rectangleDiv);
-
-    // Crear botón
-    this.backToRappiBtn = document.createElement("button");
-    this.backToRappiBtn.id = "BacktoRappibtn";
-    this.backToRappiBtn.innerText = "Back to Rappi";
-    this.backToRappiBtn.addEventListener("click", () => this.changePage());
-
-    // Agregar elementos al DOM
-    sorryDiv.appendChild(this.backToRappiBtn);
-    bodyDiv.appendChild(sorryDiv);
-    document.body.appendChild(bodyDiv);
-  }
-
-  changePage() {
-    // Cambiar de página al hacer clic en el botón
+  function cambiarPagina() {
     window.location.href = '../main-menu/main-menu.html';
   }
-}
 
-// Inicializar la clase
+  // Inicializar la clase
 new SorryScreen();
