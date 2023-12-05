47a862bb725c95b82fd0e939d10df38d6ae0fb9a
S1LB4 <113909639+S1LB4@users.noreply.github.com>
2023-12-05 12:27:56 -0500

    Add files via upload

--- player1/index.js
+++ player1/index.js
@@ -1,11 +1,15 @@
 // Importar las pantallas
 import { Takephoto } from './Screens/Takephoto/Takephoto.js';
 import { Game } from './Screens/Game/game.js';
+import { Congratulations } from './Screens/Congrats-screen/Congrats.js';
+
+import { CupponScreen } from './Screens/Cuppon-screen/Cuppon.js';
+
+import { SorryScreen } from './Screens/Sorry-screen/Sorry.js';
 
 const app = (p5) => {
-   
 
-  let currentScreen = 'game';
+  let currentScreen = 'takephoto';
   let currentScreenInstance;
   let socket;
 
@@ -15,16 +19,22 @@ const app = (p5) => {
 
     if (currentScreen === 'takephoto') {
       currentScreenInstance = new Takephoto(p5, changeScreen);
-    } 
+    }
     else if (currentScreen === 'game') {
       currentScreenInstance = new Game(p5, changeScreen);
       currentScreenInstance.keyPressed();
+    }else if (currentScreen === 'Congrats') {
+      currentScreenInstance = new Congratulations(p5, changeScreen);
+    }else if (currentScreen === 'congrats') {
+      currentScreenInstance = new CupponScreen(p5, changeScreen);
+    }else if (currentScreen === 'sorry') {
+      currentScreenInstance = new SorryScreen(p5, changeScreen);
     }
   };
 
   p5.setup = () => {
     socket = io.connect('http://localhost:5500/', {path: '/real-time'});
-    changeScreen('game');
+    changeScreen('Congrats');
 
     // socket.on('logIn', () => {
     //   changeScreen('main');
@@ -51,10 +61,13 @@ const app = (p5) => {
   p5.draw = () => {
     currentScreenInstance.draw();
 
+    if(currentScreen === 'game'){
+      p5.keyPressed = () => {
+        currentScreenInstance.keyPressed();
+      }
+    }
   };
-  p5.keyPressed = () => {
-    currentScreenInstance.keyPressed();
-  }
+
 
 };
 
