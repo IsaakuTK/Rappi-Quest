28d08d2cb4cfa2970070378223715e1e3eea73af
S1LB4 <113909639+S1LB4@users.noreply.github.com>
2023-12-04 20:08:37 -0500

    Add files via upload

--- player1/Screens/Congrats-screen/Congrats.js
+++ player1/Screens/Congrats-screen/Congrats.js
@@ -1,172 +1,75 @@
-export class Congrats {
-    constructor(p5, changeScreenCallback) {
-      constructor(p5, changeScreenCallback){
-        this.p5 = p5;
-        this.FRAME = 200;
-        this.nameInput = this.p5.createInput();
-        this.changeScreen = changeScreenCallback;
-        this.socket = io.connect('http://localhost:5500', { path: '/real-time' });
-    }
-    canvasCreation(container){
-        this.p5.createCanvas(this.FRAME, this.FRAME); // Creamos el canvas
-        let pCanvas = document.querySelector(".p5Canvas"); // seleccionamos el canvas
-        pCanvas.classList.add("my-auto", "hidden");
-        container.appendChild(pCanvas); // agregamos el canvas a el contendor
-      }
-      canvasCreation(container) {
-        this.p5.createCanvas(this.FRAME, this.FRAME); // Creamos el canvas
-        let pCanvas = document.querySelector(".p5Canvas"); // seleccionamos el canvas
-        pCanvas.classList.add("my-auto", "hidden");
-        pCanvas.style('object-fit', 'cover');
-        pCanvas.style('display', 'flex');
-        pCanvas.style('justify-content', 'center');
-        pCanvas.style('border', '5px solid #294464');
-        pCanvas.style('width', '200px');
-        pCanvas.style('height', '200px');
-        pCanvas.style('border-radius', '300px');
-        pCanvas.style('margin-block', '20px');
-        container.appendChild(pCanvas); // agregamos el canvas a el contendor
-      }
+// Importar la librería p5.js
+import p5 from 'p5';
 
-      canvasCreation(container) {
-        this.p5.createCanvas(this.FRAME, this.FRAME); // Creamos el canvas
-        let pCanvas = document.querySelector(".p5Canvas"); // seleccionamos el canvas
-        pCanvas.classList.add("my-auto", "hidden");
-        container.appendChild(pCanvas); // agregamos el canvas a el contendor
-      }
+// Definir una clase que utilizará p5.js
+class CongratsScreen {
+  constructor() {
+    // Inicializar variables necesarias
+    this.saveCouponBtn = null;
 
-    draw(){
-    }
-    createElements() {
-/////////////////////////////////////////////////////////
-        let body = this.p5.select('body');
-        body.style('background-color', '#FFFFFF');
-        // Crear elementos del encabezado
-        let header = this.p5.createDiv("");
-        header.class("head");
-        header.style('display', 'flex');
-        header.style('flex-direction', 'column');
-        header.style('align-items', 'center');
-        header.style('color', '#fe3f23');
-        header.style('font-family', 'Poppins, sans-serif');
-        header.style('font-size', '15px');
+    // Configurar funciones de p5.js
+    new p5(this.sketch);
+  }
 
-      let saveButton = this.p5.createButton("");
-      saveButton.class("back");
-      saveButton.mousePressed(this.backMain);
-      this.applysaveButtonStyles(saveButton);
-////////////////////////////////////////////////////////
-
-
-      let title = this.p5.createElement("h1", "Photo");
-      let number = this.p5.createElement(
-        "p",
-        "Take a photo to identify yourself in the competition."
-      );
-
-
-      // Agregar elementos al DOM
-      document.body.appendChild(backButton.elt);
-      document.body.appendChild(header.elt);
-      header.child(title);
-      header.child(number);
-      document.body.appendChild(photoDiv.elt);
-      photoDiv.child(profileImage);
-
-
-      let nextScreen = this.p5.createButton("Next");
-      nextScreen.mousePressed(this.moveMaze);
-      nextScreen.class("buttonPhoto");
-      this.applyButtonStyles(nextScreen);
-
-      //menuDiv.parent("body");
-      takePhoto.parent(menuDiv);
-      nextScreen.parent(menuDiv);
-
-      let logoDiv = this.p5.createDiv("");
-      logoDiv.class("logo-div");
-      logoDiv.style('display', 'flex');
-      logoDiv.style('flex-direction', 'column');
-      logoDiv.style('align-items', 'center');
-      logoDiv.style('margin', '30px');
-
-      let logo = this.p5.createImg("./Screens/imgs/rappi-logo-2.png", "Rappi Logo");
-      logo.style("max-width", "100%");
-      logo.style("height", "auto");
-      logo.style("margin-bottom", "20px");
-
-      logo.parent(logoDiv);
-    }
-
-    moveMaze = () => {
-      // Get the value from the nameInput
-      const playerName = this.nameInput.value();
-
-      // Check if the playerName is not empty before proceeding
-      if (playerName.trim() !== "") {
-        // Proceed with your logic here
-      } else {
-        alert("Please enter your name before proceeding.");
-      }
+  // Función de configuración de p5.js
+  sketch(p) {
+    p.setup = () => {
+      // Crear elementos y configurar eventos cuando el DOM esté completamente cargado
+      p.createCanvas(window.innerWidth, window.innerHeight);
+      this.createHTML();
     };
+  }
 
-    applyButtonStyles(button) {
-      button.style('background-color', '#FF4B3D');
-      button.style('color', '#fff');
-      button.style('font-family', 'Poppins, sans-serif');
-      button.style('font-size', '22px');
-      button.style('font-weight', 'bolder');
-      button.style('margin-top', '10px');
-      button.style('width', '258px');
-      button.style('height', '49px');
-      button.style('border-radius', '40px');
-      button.style('border', 'none');
-      button.style('outline', 'none');
-      button.style('cursor', 'pointer');
-      button.style('transition', 'background-color 0.3s');
+  createHTML() {
+    // Crear div contenedor
+    let bodyDiv = document.createElement("div");
+    bodyDiv.classList.add("body");
 
-      button.mouseOver(() => {
-        button.style('border', '2px solid #ff4b3d');
-        button.style('background-color', '#fff');
-        button.style('color', '#ff4b3d');
-      });
+    // Crear div de felicitaciones
+    let congratsDiv = document.createElement("div");
+    congratsDiv.classList.add("CONGRATS");
 
-      button.mouseOut(() => {
-        button.style('border', 'none');
-        button.style('background-color', '#ff4b3d');
-        button.style('color', '#fff');
-      });
+    // Crear div del rectángulo
+    let rectangleDiv = document.createElement("div");
+    rectangleDiv.classList.add("rectangulo");
 
-      button.mousePressed(() => {
-        button.style('transform', 'scale(0.95)');
-      });
+    // Crear elementos internos del rectángulo
+    let title = document.createElement("h1");
+    title.innerText = "Congratulations!";
+    let paragraph = document.createElement("p");
+    paragraph.innerText =
+      "You completed the rappi quest with a new time mark, you won the discount code for the restaurant in where you scanned the QR";
+    let orangeCheck = document.createElement("img");
+    orangeCheck.classList.add("orangecheck");
+    orangeCheck.src = "./IMAGES/Check.jpg";
+    orangeCheck.alt = "";
+    let couponCode = document.createElement("h3");
+    couponCode.innerText = "7269-1a19-1be1-082n";
 
-      button.mouseReleased(() => {
-        button.style('transform', 'scale(1)');
-      });
-    }
-/////////////////////////////////////////////////////////
-    applyBackButtonStyles(button) {
-      button.style('background-color', '#294464');
-      button.style('border-radius', '30px');
-      button.style('width', '70px');
+    // Agregar elementos al DOM
+    rectangleDiv.appendChild(title);
+    rectangleDiv.appendChild(paragraph);
+    rectangleDiv.appendChild(orangeCheck);
+    rectangleDiv.appendChild(couponCode);
+    congratsDiv.appendChild(rectangleDiv);
 
-      button.mouseOver(() => {
-        button.style('background-color', '#002046');
-        button.style('color', '#fff');
-      });
+    // Crear botón
+    this.saveCouponBtn = document.createElement("button");
+    this.saveCouponBtn.id = "saveCouponBtn";
+    this.saveCouponBtn.innerText = "Save Your Coupon";
+    this.saveCouponBtn.addEventListener("click", () => this.changePage());
 
-      button.mouseOut(() => {
-        button.style('background-color', '#294464');
-        button.style('color', '');
-      });
+    // Agregar elementos al DOM
+    congratsDiv.appendChild(this.saveCouponBtn);
+    bodyDiv.appendChild(congratsDiv);
+    document.body.appendChild(bodyDiv);
+  }
 
-      button.mousePressed(() => {
-        button.style('transform', 'scale(0.95)');
-      });
+  changePage() {
+    // Cambiar de página al hacer clic en el botón
+    window.location.href = '../Cuppon-screen/p5.html';
+  }
+}
 
-      button.mouseReleased(() => {
-        button.style('transform', 'scale(1)');
-      });
-    }
-  }}
+// Inicializar la clase
+new CongratsScreen();
